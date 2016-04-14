e0800f552df288e7dc7fe4cf889646babed0071d
Chris Foral <chris.foral@gmail.com>
2016-04-14 16:03:51 -0500

    Defining swear_check method with Regex. Getting close

--- app/models/swear_library.rb
+++ app/models/swear_library.rb
@@ -1,8 +1,6 @@
 class SwearLibrary < ActiveRecord::Base
-
-  def swear_check
     # User types words (string) into a text field (comment, message, email, etc) on a web browser
-    # In order for this check to take place, the user must submit the string they've typed (submit comment, message, send email, etc)
+    # In order for this check to take place, the user must submit the string they've typed (submit comment or message, send email, etc)
     # The submitted string could be a single word or many paragraphs
     # This method must identify the single word as a swear or parse through each word to identify a swear
     # If a swear is found, the swear(s) will be pushed to an array
@@ -10,6 +8,18 @@ class SwearLibrary < ActiveRecord::Base
     # (A hash could be made with the current user ID as the key and the value being the swear array)
     # This array (or hash) is what will be returned from this method
     # That array (or hash) will be passed to the swear_logs controller action
-    # That controller action will loop through the array
+    # That controller action will loop through the array (or hash)
+    # A swear_log
+  def self.swear_check(user_swear)
+    swear_arr = []
+    final_swear = []
+
+    SwearLibrary.all.each do |lib|
+     swear_arr << lib.swear
+    end
+    swear_arr.each do |swear|
+      final_swear << /#{swear}/.match(user_swear)
+    end
+    final_swear.compact
   end
 end
