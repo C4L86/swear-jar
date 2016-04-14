893580492f44da565cb78240c6221546823c8e75
Chris Foral <chris.foral@gmail.com>
2016-04-14 11:12:50 -0500

    Small changes

--- app/controllers/swear_logs_controller.rb
+++ app/controllers/swear_logs_controller.rb
@@ -6,10 +6,12 @@ class SwearLogsController < ApplicationController
   def log_swear
     @swear = params[:swear]
     @user  = current_user
-
+    binding.pry
     @logged_swear          = SwearLog.new
     @logged_swear.user_id  = @user.id
-    @logged_swear.swear_id = SwearLibrary.where({"swear" => @swear})
+    @logged_swear.swear_id = SwearLibrary.find_by_swear(@swear).id
+    @logged_swear.swear_cost_id = 5
+    @logged_swear.save
 
   end
 
